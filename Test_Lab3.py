db4e2fc38637e860ee3bcb00090e1cc4dc1e2821
Lucas <lucaswai.21@ichat.sp.edu.sg>
2022-05-04 19:33:48 +0800

    ......

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -5,26 +5,42 @@ print("Test_Lab3")
 
 def test_bubble_sort_ascending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [11, 12, 22, 25, 34, 64, 90]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 23, 12, 32]
+    test_arr = [11, 12, 12, 22, 23, 25, 32, 34, 64, 90]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
 
     assert (result == test_arr)
 
+
 def test_bubble_sort_descending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [90, 64, 34, 25, 22, 12, 11]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 32, 12, 23]
+    test_arr = [90, 64, 34, 32, 25, 23, 22, 12, 12, 11]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
     assert (result == test_arr)
 
-def test_bubble_sort_invalid():
+
+def test_bubble_sort_above10():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 36, 53, 43, 15, 53]
     result = Lab3.bubble_sort(input_arr, 3)
+    assert (result == 1)
 
-    assert (result == [])
+
+def test_bubble_sort_below10():
+        result = []
+        input_arr = [64, 34, 25, 12, 22, 11, 90]
+        result = Lab3.bubble_sort(input_arr, 3)
+        assert (result == 2)
+
+
+def test_bubble_sort_zero():
+    result = []
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, 3)
+    assert (result == 0)
+
+
